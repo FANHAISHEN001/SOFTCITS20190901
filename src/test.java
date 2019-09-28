d4f0c944b83140343eca95ca19afddccd49981b3
FANHAISHEN001 <861561091@qq.com>
2019-09-28 11:02:50 +0800

    第一次push
    练习
    *.classpath
    .project
    .settings\org.eclipse.jdt.core.prefs

---
+++ src/test.java
@@ -0,0 +1,57 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		println();	
+	}		
+    /*	print();
+		print1();
+		print2();
+		print3();		
+	//单行添加注释	  
+     * 多行注释方法
+     * 	花括号为方法体 		
+	public static void print() {
+		System.out.println("Hello World!");
+	}
+	public static void print1() {
+		System.out.println("Hello World!111");
+	}
+	public static void print2() {
+		System.out.println("Hello World!222");
+	}
+	public static void print3() {
+		System.out.println("Hello World!333");
+	}
+    */		
+	public static void println() {
+	//变量声明		
+	int	a;
+	int b;
+	//变量定义
+	a=2;
+	b=3;
+	//声明和定义同时做
+	int c=a+b;
+	System.out.println(c);
+	
+	}
+	
+	
+	   
+	}		
+	
+
+
+
+
+
+
+
+
+
+
+
+
